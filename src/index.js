cb7a92c0126c8a648ad16c5b8bcdbd9a56d73c98
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-09-13 18:28:50 +0300

    added phoneMask

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 import headerInterective from './modules/headerInterective';
 import lowScroll from './modules/lowScroll';
 import popupLinkList from './modules/popupLinkList';
+import maskPhone from './modules/maskPhone';
 
 
 headerInterective();
 lowScroll();
 popupLinkList();
+maskPhone();
