b681fcfa1b8a6bf1caa4f966fbf3833effc03a25
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-09-15 23:55:45 +0300

    fixed bag admin

--- layout/admin/scriptTable.js
+++ layout/admin/scriptTable.js
@@ -1,8 +1,23 @@
 'use strict';
 
-const parentPage = document.referrer,
-  cookies = document.cookie;
+// const getData = () => {
+//   return fetch('http://localhost:3000/api/items/6370095354');
+// };
 
-if (parentPage === '' && !cookies) {
-  window.location.href = './index.html';
-}
+// const parseData = (data) => {
+//   const dataRepair = JSON.parse(data);
+//   console.log(dataRepair);
+// };
+
+// getData()
+// .then((response) => {
+//   if (response.ok) {
+//     return response.text();
+//   } else {
+//     throw new Error('status networn not 200');
+//   }
+// })
+// .then(parseData)
+// .catch(error => {
+//   console.warn(error);
+// });
